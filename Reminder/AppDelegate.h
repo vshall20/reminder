e10c93f30bb67b7a137fa0acdfaf0d9d54088990
Vishal Dharmawat <vishal.dharmawat@gmail.com>
2013-06-30 17:00:08 +0530

    Initial Commit

---
+++ Reminder/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Reminder
+//
+//  Created by Vishal Dharmawat on 30/06/13.
+//  Copyright (c) 2013 Vishal Dharmawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Reminder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Reminder
+//
+//  Created by Vishal Dharmawat on 30/06/13.
+//  Copyright (c) 2013 Vishal Dharmawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
